9ea336f7ec0ab0a5f42cd429718b42873d9ee560
ChoiYoungWoo2197 <duddn2197@naver.com>
2019-12-05 13:38:06 +0900

    first commit

--- src/main/java/co/kr/controller/ShopController.java
+++ src/main/java/co/kr/controller/ShopController.java
@@ -1,18 +1,19 @@
 package co.kr.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
-
+import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.RestTemplate;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Handles requests for the application home page.
@@ -26,26 +27,25 @@ public class ShopController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "orders" , method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		//logger.info("shop call {}.", locale);
-		System.out.println("shop before");
-		
-		
-		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<String> responseEntity = restTemplate.getForEntity("http://127.0.0.1:9220/controller/api/orders", String.class);
+	public String shop(Locale locale, Model model, HttpServletRequest request) {
+		try {
+			logger.info("shop call {}.", locale);
+			String authorityId = request.getParameter("authorityid");
+						
+			RestTemplate restTemplate = new RestTemplate();
+			ResponseEntity<String> responseEntity = restTemplate.getForEntity("http://127.0.0.1:9220/controller/api/orders?authorityid="+authorityId, String.class);
 
+			ObjectMapper mapper = new ObjectMapper();
+			List<Object> jsonlist =  new ArrayList<Object>();
+			jsonlist = mapper.readValue(responseEntity.getBody(), new TypeReference<ArrayList<Object>>(){});
 
-		System.out.println("shop after");
+			model.addAttribute("totalData", jsonlist);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 		
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("totalData", responseEntity.getBody() );
-		
-		return "home";
+		return "orders";
 	}
 	
 }
---
+++ target/controller-1.0.0-BUILD-SNAPSHOT/WEB-INF/views/orders.jsp
@@ -0,0 +1,33 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+<head>
+	<title>cmt_spring_level_check</title>
+	<link rel="stylesheet" href="/resources/css/style.css" />
+</head>
+<body>
+<h1>
+	주문 현항 테이블
+</h1>
+
+<table class= "bluetop">
+ <tr>
+    <th>주문 식별번호</th>
+    <th>고객 식별번호</th>
+    <th>고객 이름</th>
+    <th>상품 식별번호</th>
+    <th>상품명</th>
+  </tr>    
+<c:forEach var="data" items="${totalData}">
+  <tr>
+    <th>${data.orderId}</th>
+    <th>${data.customId}</th>
+    <th>${data.customName}</th>
+    <th>${data.productId}</th>
+    <th>${data.productName}</th>
+  </tr>    
+</c:forEach>
+</table>
+</body>
+</html>
